3030c00e2f580c5a44d33d39bb28ec7ea186f95f
Md Tanzeel Siddiqui <siddtanzeel@gmail.com>
2026-09-01 21:13:14 +0530

    Create 5_Deletion_from_the_beginning.cpp

---
+++ 5_Deletion_from_the_beginning.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+};
+
+int main()
+{
+    Node* head = NULL;
+    Node* temp;
+    Node* new_node;
+    
+    int n;
+    cout<<"Enter number of node: ";
+    cin>>n;
+
+    for(int i=0;i<n;i++)
+    {
+        new_node = new Node();
+        cout<<"Enter data: ";
+        cin>>new_node->data;
+        new_node->next = NULL;
+
+        if(head == NULL)
+            head = new_node;
+        else
+        {
+            temp = head;
+            while(temp->next != NULL)
+                temp = temp->next;
+            temp->next = new_node;
+        }
+    }
+
+    cout<<"Original Linked List"<<endl;
+    temp = head;
+    while(temp != NULL)
+    {
+        cout<<temp->data<<" -> ";
+        temp = temp->next;
+    }
+    cout<<"NULL"<<endl;
+
+    temp = head;
+    head = head->next;
+    delete temp;
+
+    cout<<"Updated Linked List"<<endl;
+    temp = head;
+    while(temp != NULL)
+    {
+        cout<<temp->data<<" -> ";
+        temp = temp->next;
+    }
+    cout<<"NULL"<<endl;
+    return 0;
+}
