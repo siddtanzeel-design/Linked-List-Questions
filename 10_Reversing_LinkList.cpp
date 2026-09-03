4648f154a63c6fc0b584912723c3bae7c78a41f4
Md Tanzeel Siddiqui <siddtanzeel@gmail.com>
2026-09-03 13:59:17 +0530

    Implement linked list creation and reversal
    This program creates a linked list, displays it, reverses the list, and displays the reversed list.

---
+++ 10_Reversing_LinkList.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+};
+Node* createList(Node* head)
+{
+    Node* temp;
+    Node* new_node;
+
+    int n;
+    cout<<"Enter number of nodes: ";
+    cin>>n;
+
+    for(int i=0;i<n;i++)
+    {
+        new_node = new Node();
+
+        cout<<"Enter data: ";
+        cin>>new_node->data;
+        new_node->next = NULL;
+
+        if(head == NULL)
+            head = new_node;
+        else
+        {
+            temp = head;
+            while(temp->next != NULL)
+                temp = temp->next;
+            temp->next = new_node;
+        }
+    }
+    return head;
+}
+void displayList(Node* head)
+{
+    Node* temp = head;
+    while(temp != NULL)
+    {
+        cout<<temp->data<<" -> ";
+        temp = temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+Node* reverseList(Node* head)
+{
+    Node* prev = NULL;
+    Node* temp = head;
+    Node* next;
+
+    while(temp != NULL)
+    {
+        next = temp->next;
+        temp->next = prev;
+        prev = temp;
+        temp = next;
+    }
+    return prev;
+}
+int main()
+{
+    Node* head = NULL;
+
+    head = createList(head);
+    cout<<"Origninal Link List"<<endl;
+    displayList(head);
+
+    head = reverseList(head);
+    cout<<"Reversed Link List"<<endl;
+    displayList(head);
+
+    return 0;
+}
